2cff62a1c78af3b00cb895c8e2bee24f04bd2327
abdo <abderrazakbourial0@gmail.com>
2022-03-31 14:56:28 +0000

    a

---
+++ pages/nft/[id].tsx
@@ -0,0 +1,20 @@
+export default function NFTDropPage() {
+  return (
+    <div className="h-screen flex flex-col">
+      {/* Left Side */}
+      <div className="bg-gradient-to-br from-cyan-800 to-rose-500">
+        <div>
+          <div className="bg-gradient-to-br from-yellow-500">
+            <img className="w-44 rounded-xl object-cover lg:h-96 lg:w-72" src="https://links.papareact.com/8sg" alt="" />
+          </div>
+          <div>
+            <h1 className="text-4xl font-bold text-white">PAPAFAMS Apes</h1>
+            <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, itaque.</h2>
+          </div>
+        </div>
+      </div>
+
+      {/* Right Side */}
+    </div>
+  )
+}
